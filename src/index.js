7d0f74d5051700776d1e15f468a670a1940909b0
zaiats85 <oleg.zaiats@perfectial.com>
2019-06-12 18:19:14 +0300

    done

--- src/index.js
+++ src/index.js
@@ -8,12 +8,57 @@
  */
 
 const rangeArray = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
-const lastItem = (arr) => {
-    if (!Array.prototype.last){
-        Array.prototype.last = function(){
-            return this[this.length - 1];
-        };
+const checkType = arr => {
+    if (!Array.isArray(arr)) {
+        throw "wrong input";
     }
+};
+const getMax = (arr) => Math.max(...arr);
+const getMin = (arr) => Math.min(...arr);
+
+Set.prototype.isSuperset = function(subset) {
+    for (var elem of subset) {
+        if (!this.has(elem)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+Set.prototype.union = function(setB) {
+    let union = new Set(this);
+    for (let elem of setB) {
+        union.add(elem);
+    }
+    return union;
+};
+
+Set.prototype.nextTo = function(setB) {
+    for (let elem of setB) {
+        if (this.has(elem + 1) || this.has(elem - 1)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+Set.prototype.intersection = function(setB) {
+    let intersection = new Set();
+
+    for (let elem of setB) {
+        if (this.has(elem)) {
+            intersection.add(elem);
+        }
+    }
+    return intersection;
+};
+
+Set.prototype.difference = function(setB) {
+    var difference = new Set(this);
+    for (var elem of setB) {
+        difference.delete(elem);
+    }
+    return difference;
 };
 
 
@@ -27,70 +72,73 @@ class RangeList {
         this.rangeList = [];
     }
 
-    static lastItem(arr) {
-        return arr[arr.length - 1];
-    };
-
-    checkRemove(min, max){
+    checkToRemove(min, max){
         let k = this.rangeList;
         let m = k.length;
-        if(!m) return;
+        let i = 0;
+        if(max - min <= 0) return;
 
-        for(let i = 0; i < m; i++){
-            let range = k[i];
+        let testSet = rangeArray(min, max);
+        let result = [];
 
-            if(range.includes(min) && range.includes(max)){
-                console.log("swallow");
-                return;
-            } else if(range.includes(min) && max > RangeList.lastItem(range)){
+        while(i < m){
 
-                console.log("extend max");
+            if(k[i].intersection(new Set(testSet)).size){
 
-            } else if(range.includes(max) && min < range[0]){
+                let range = [...k[i]];
+                let upperBound = getMax(range) + 1;
 
-                console.log(min + " " + range[0]);
+                let f1 = rangeArray(getMin(range), min);
+                let f2 = rangeArray(max, upperBound);
 
-                console.log("extend min");
+                if(f1.length > 1){
+                    result.push(new Set(f1));
+                }
+                if(f2.length > 1){
+                    result.push(new Set(f2));
+                }
 
+            } else {
+                result.push(k[i]);
             }
 
+            i++;
         }
 
+        this.rangeList = result;
     }
 
-    checkIncludes(min, max){
+    checkToAdd(min, max){
         let k = this.rangeList;
         let m = k.length;
+        let i = 0;
 
-        if( Math.abs(max - min) <= 0) return;
+        if(max - min <= 0) return;
+        let testSet = new Set(rangeArray(min, max));
 
-        for(let i = 0; i < m; i++){
-            let range = k[i];
-
-            if(range.includes(min) && range.includes(max)){
-                console.info("swallow");
-                return;
-            } else if(range.includes(min-1) && max > RangeList.lastItem(range)){
-                console.log("extend max " + RangeList.lastItem(range) + " to " + max);
-                k[i] = range.concat(rangeArray(min, max));
-                return;
-            } else if(range.includes(max) && min < range[0]){
-                console.log("extend min " + range[0] + " to " + min);
-                k[i] = range.concat(rangeArray(min, range[0]));
-                return;
+        while(i < m){
+            if(k[i].isSuperset(testSet)){
+                return false;
+            } else if(k[i].nextTo(testSet)){
+                return k[i] = k[i].union(testSet);
             }
+            i++;
         }
 
-        this.rangeList.push(rangeArray(min, max));
+        this.rangeList.push(testSet);
     }
 
     // TODO: implement this
     add(range) {
-        if (!Array.isArray(range)) {
-            console.warn("wrong input");
+
+        try {
+            checkType(range)
+        } catch (e) {
+            console.warn(e);
+            return
         }
 
-        this.checkIncludes(range[0], range[1]);
+        this.checkToAdd(range[0], range[1]);
 
     }
 
@@ -98,15 +146,31 @@ class RangeList {
      * Removes a range from the list
      * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
      */
+    // TODO: implement this
     remove(range) {
-        // TODO: implement this
+        try {
+            checkType(range)
+        } catch (e) {
+            console.warn(e);
+            return
+        }
+
+        this.checkToRemove(range[0], range[1]);
+
     }
 
     /**
      * Prints out the list of ranges in the range list
      */
+    // TODO: implement this
     print() {
-        // TODO: implement this
+        let str = "";
+        this.rangeList.forEach(set => {
+            let max = getMax(set) + 1;
+            let min = getMin(set);
+            str += `[${min},${max}) `;
+        });
+        console.log(str);
     }
 }
 
@@ -114,47 +178,52 @@ class RangeList {
 const rl = new RangeList();
 
 rl.add([1, 5]);
-rl.print();
+//rl.print();
 // Should display: [1, 5)
 
 rl.add([10, 20]);
-rl.print();
+//rl.print();
 // Should display: [1, 5) [10, 20)
+
+rl.add([9, 10]);
+//rl.print();
+// Should display: [1, 5) [9, 20)
 
 rl.add([20, 20]);
-rl.print();
-// Should display: [1, 5) [10, 20)
+//rl.print();
+// Should display: [1, 5) [9, 20)
 
 rl.add([20, 21]);
-rl.add([21, 22]);
-rl.add([24, 28]);
-rl.add([24, 21]);
-rl.print();
-// Should display: [1, 5) [10, 21)
+//rl.print();
+// Should display: [1, 5) [9, 21)
 
 rl.add([2, 4]);
-rl.print();
-// Should display: [1, 5) [10, 21)
+//rl.print();
+// Should display: [1, 5) [9, 21)
 
 rl.add([3, 8]);
-rl.print();
-// Should display: [1, 8) [10, 21)
+//rl.print();
+// Should display: [1, 8) [9, 21)
 
-/*rl.remove([10, 10]);
+rl.remove([10, 10]);
 rl.print();
-// Should display: [1, 8) [10, 21)
+// Should display: [1, 8) [9, 21)
 
-rl.remove([10, 11]);
+rl.remove("asdfasdfasdf");
+// Should throw: wrong input
+
+rl.remove([9, 11]);
 rl.print();
 // Should display: [1, 8) [11, 21)
 
 rl.remove([15, 17]);
 rl.print();
 // Should display: [1, 8) [11, 15) [17, 21)
+
+
 rl.add('asdfasd');
+// Should throw: wrong input
 
 rl.remove([3, 19]);
-rl.print();*/
-
-console.log(rl);
+rl.print();
 // Should display: [1, 3) [19, 21)
